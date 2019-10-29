c91bd620548275edb28af90d48848c48337ede8b
kmurata798 <kmurata798@gmail.com>
2019-10-28 17:57:53 -0700

    basis of analyze words.py completed

---
+++ Code/analyze_words.py
@@ -0,0 +1,71 @@
+# HISTOGRAM FUNCTION
+def read_file(file):
+    """Reads from file"""
+    with open(file, 'r') as f:
+        words = f.read().split()
+        words = [text.lower() for text in words]
+    return words
+
+
+def histogram(file):
+    """return a histogram data structure [list] that stores each unique word
+    along with the number of times the word appears in the source text.
+    !!!TAKES VERY LONG TO LOAD!!!"""
+
+    text = read_file(file)
+    histogram = []
+
+    for word in text:
+        exists = False
+        for hWord in histogram:
+            if hWord[0] == word:
+                hWord[1] += 1
+                exists = True
+        if exists == False:
+            histogram.append([word, 1])
+    return histogram
+
+
+def histogram_dict(file):
+    """return a histogram data structure that stores each unique word 
+    along with the number of times the word appears in the source text"""
+
+    words = read_file(file)
+    histogram = {}
+    for word in words:
+        if word in histogram:
+            histogram[word] += 1
+        else:
+            histogram[word] = 1
+    return histogram
+
+# UNIQUE WORDS FUNCTION
+
+
+def unique_words(histogram):
+    """returns the total count of unique words with dictionary"""
+    return len(histogram)
+
+# #FREQUENCY FUNCTION
+
+
+def frequency(word, histogram):
+    """returns the number of times that word appears in a text."""
+
+    for key, value in histogram.items():
+        if key == word:
+            return value
+
+
+if __name__ == "__main__":
+    histogram = histogram_dict("txtdocs/sherlock.txt")
+    # histogram = histogram("txtdocs/sherlock.txt")
+
+    unique_words = unique_words(histogram)
+    word = 'mystery'
+    word_frequency = frequency(word, histogram)
+
+
+    print(histogram)
+    print(f'Unique word count: {unique_words}')
+    print(f"Number of times '{word}' appeared in the text: {word_frequency}")
--- Code/random_word.py
+++ Code/random_word.py
@@ -1,43 +1,40 @@
-import randomimport sys
+import random
+import sys
 
 def get_file_lines(filename):
     '''OPEN the FILE _WITHIN_ a function, the you DON'T HAVE TO TYPE file.close()!!!!!'''
     with open(filename, 'r') as file:
-        file.readlines()
+        print(file.readlines())
     
-    blablah
 
+    # file = open(filename, 'r')
+
+    # lines = []
+    # for line in file:
+    #     lines.append(line.strip())
+
+    lines = file.readlines() 
+    '''THIS does the above 3 lines of code at once'''
+    # clean_lines = []
+    # for line in lines:
+    #     clean_lines.append(line.strip())
+    
+    #list comprehension LOOKUP
+    # clean_lines = [line.strip() for line in lines] 'THIS does the above three lines of code at once'
+    # strip() gets rid of white space at beginning/end
+    # file.close()
     return lines
 
-    file = open(filename, 'r')
-
-    lines = []
-    for line in file:
-        lines.append(line.strip())
-
-        line = file.readlines() 'THIS does the above 3 lines of code at once'
-
-        clean_lines = []
-        for line in lines:
-            clean_lines.append(line.strip())
-        
-        #list comprehension LOOKUP
-        clean_lines = [line.strip() for line in lines] 'THIS does the above three lines of code at once'
-
-        # strip() gets rid of white space at beginning/end
-        # file.close()
-        return lines
-
 def pick_random_word(word_list):
-    index = random.randint(0. len(word_list) - 1)
-    index = random.randint(len(word_list))
+    index = random.randint(0, len(word_list) - 1)
+    # index = random.randint(len(word_list))
     return word_list[index]
 
 def main():
     dictionary_words = get_file_lines('/usr/share/dic/words')
     if len(sys.argv) >= 2:
         num_words = int(sys.argv[1])
-        for iteration in range(num_words):
+        for _ in range(num_words):
             word = pick_random_word(dictionary_words)
             print(word)
     else:
