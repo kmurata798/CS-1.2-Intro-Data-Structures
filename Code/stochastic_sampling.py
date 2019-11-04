942882036593cd4f68d6e69666f98e81599de144
kmurata798 <kmurata798@gmail.com>
2019-11-04 01:51:43 -0800

    venv and flask progress

--- Code/stochastic_sampling.py
+++ Code/stochastic_sampling.py
@@ -10,12 +10,45 @@ def pick_random_word(histogram):
     return key[random_index]
 
 def sample_weight(histogram):
+    """Uses the histogram dictionary to add each of its keys 
+    to a new list based on the keys' value"""
     freq_list = []
     for key, value in histogram.items():
         [freq_list.append(key) for index in range(value)]
     rand_num = random.randint(0, len(freq_list) - 1)
     return freq_list[rand_num]
     
+def test_sample_weight():
+    """Test sample_weight function"""
+    freq_dict = {}
+    words = histogram_dict('../Code/txtdocs/test.txt')
+    for i in range(10000):
+        sample_word = sample_weight(words)
+        if sample_word in freq_dict:
+            freq_dict[sample_word] += 1
+        else:
+            freq_dict[sample_word] = 1
+    """output key words and their probability percentages"""
+    for i in freq_dict:
+        # print(i, frequency_dict[i])
+        print(i, freq_dict[i] / 10000)
+
+    return freq_dict
+
+def get_sentence(histogram, amount=15):
+    '''Uses the sample_weight function to get weighted words and
+    combine them in a sentence
+    PARAMETERS:
+    histogram: Dictionary
+    amount: int (default = 15 words)
+    '''
+    words = []
+    for i in range(amount):
+        words.append(sample_weight(histogram))
+    sentence = ' '.join(words)
+
+    return sentence
+
 def testing_random():
     onecount = 0
     twocount = 0
@@ -42,7 +75,9 @@ if __name__ == "__main__":
     # testing_random()
     entry = f"../Code/txtdocs/{sys.argv[1]}"
     texts = histogram_dict(entry)
-    print(pick_random_word(texts))
-    print(sample_weight(texts))
+    # print(pick_random_word(texts))
+    # print(sample_weight(texts))
+    # test_sample_weight()
+    print(get_sentence(texts, 20))
 
 
---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, redirect, url_for
+from pymongo import MongoClient
+from Code.stochastic_sampling import get_sentence
+from Code.analyze_words import histogram_dict
+import os
+
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/CS-1.2-Intro-Data-Structures')
+client = MongoClient(host=f'{host}?retryWrites=false')
+db = client.get_default_database()
+trucks = db.trucks
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """returns user to the homepage"""
+    words = 'Code/txtdocs/fish.txt'
+    histogram = histogram_dict(words)
+    sentence = get_sentence(words, 25)
+    return render_template("home.html", tweet=sentence)
+
+if __name__ == "__main__":
+    app.run(Debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
