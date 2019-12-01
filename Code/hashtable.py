f66a1ee3b90997169affc3f41d11f0c453f96233
kmurata798 <kmurata798@gmail.com>
2019-12-01 15:50:08 -0800

    hashtable values func

--- Code/hashtable.py
+++ Code/hashtable.py
@@ -37,8 +37,13 @@ class HashTable(object):
     def values(self):
         """Return a list of all values in this hash table.
         TODO: Running time: O(???) Why and under what conditions?"""
-        # TODO: Loop through all buckets
-        # TODO: Collect all values in each bucket
+        # Loop through all buckets
+        # Collect all values in each bucket
+        all_values = []
+        for bucket in self.buckets:
+            for key, value in bucket.items():
+                all_values.append(value)
+        return all_values
 
     def items(self):
         """Return a list of all items (key-value pairs) in this hash table.
