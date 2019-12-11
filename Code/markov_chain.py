c65ba2d62697a270a61b233fb79f25494ea1170b
kmurata798 <kmurata798@gmail.com>
2019-12-11 14:15:07 -0800

    poe

--- Code/markov_chain.py
+++ Code/markov_chain.py
@@ -29,19 +29,19 @@ class Markov():
         next_words = []
         next_pairs = []
 
-        for a in range(len(self.word_list)-1): 
-            """loop through each word in word_list"""
-            words.clear()
+        for a in range(len(self.word_list) -  1): 
+            #loop through each word in word_list
+            words = []
             for b in range(self.order):
-                """order --> # of Markov Order"""
+                #order --> # of Markov Order
                 if a < (len(self.word_list) - self.order):
-                    """checks if given words are would be outside the range of word_list"""
+                    #checks if given words are would be outside the range of word_list
                     words.append(self.word_list[a + b])
             if words == focus_words:
-                """If the words inside word_list matches our focus words..."""
-                next_words.clear()
+                #If the words inside word_list matches our focus words...
+                next_words = []
                 for b in range(self.order):
-                    """Append next words and combines them into a string"""
+                    #Append next words and combines them into a string
                     next_words.append(self.word_list[a + (b + 1)])
                 next_words_string = ' '.join(next_words)
                 next_pairs.append(next_words_string)
@@ -54,52 +54,105 @@ class Markov():
 
         word_list = str
         order = int
+        """
+        next_words = []
+        main_histogram = Dictogram(self.word_list)
 
+        #Collect initial starting words
+        next_word = main_histogram.sample()
+        next_words.append(next_word)
+        chain = self.next_chain(next_word)
+
+        #Chooses additional words based on the Markov order
+        for index in range(self.order - 1):
+            if len(chain) > 0:
+                following_word = chain.sample()
+                next_words.append(following_word)
+                chain = self.next_chain(following_word)
+
+        sample = ' '.join(next_words)
+        return sample
+
+    def higher_walk(self):
+        """Starts by using the initial sample words to begin the chain.
+        Then it uses the higher order chain to generate a full sentence using the order #.
+        Amount with equal the length of the sentence.
+        
+        word_list = str
+        amount = int
+        order = int
+        """
+        sentence = []
+        next_words = []
+
+        #Declare and initialize starting sample words.
+        words_str = self.order_sample()
+        sentence.append(words_str)
+
+        # Create the full sentence given the amount number
+        for index in range(self.amount - self.order):
+            next_words = []
+            chain = self.higher_order(words_str) #Returns n order length of words
+            if len(chain[words_str]) > 1:
+                words_str = chain[words_str].sample()
+                next_words = words_str.split()
+                sentence.append(next_words[self.order - 1]) #Only get one item from next words to not repeat center word.
+
+        sentence = " ".join(sentence)
+        return sentence
                     
+    def next_chain(self, new_word):
+        """If the word found is equal to next_word, append the word to the list.
+        Create list of following words.
 
-def next_chain(word_list, new_word):
-    """If the word found is equal to next_word, append the word to the list.
-    Create list of following words.
+        word_list = list
+        new_word = string"""
 
-    word_list : list
-    new_word : string"""
-
-    markov_list = []
-    for index in range(len(word_list) - 1):
-        if new_word == word_list[index]:
-            markov_list.append(word_list[index + 1])
+        markov_list = []
+        for index in range(len(self.word_list) - 1):
+            if new_word == self.word_list[index]:
+                markov_list.append(self.word_list[index + 1])
+        chain = Dictogram(markov_list)
+        return chain
     
-    chain = Dictogram(markov_list)
-    return chain
+    def make_sentence(self, words):
+        """Takes each word in the list and joins them together 
+        into a single sentence + period at the end.
 
-def traverse(word_list, amount):
-    """creates a sentence starting with a random word from the first histogram.
-    The function keeps selecting a random word in each new histogram to create a new list of words.
+        words = list
+        """
+
+        split_words = words.split()
+        split_words[0] = split_words[0].capitalize()
+        f_sentence = ' '.join(split_words) + '.'
+        
+        return f_sentence
     
-    word_list : list
-    amount : integer"""
+    def main(self):
+        words = self.higher_walk()
+        sentence = self.make_sentence(words)
+        return sentence
 
-    sentence = []
-    starting_histogram = Dictogram(word_list)
-    next_word = starting_histogram.sample()
-    sentence.append(next_word)
-    for index in range((amount) - 1):
-        chain = next_chain(word_list, next_word)
-        if len(chain) > 0:
-            next_word = chain.sample()
-            sentence.append(next_word)
+# def traverse(word_list, amount):
+#     """creates a sentence starting with a random word from the first histogram.
+#     The function keeps selecting a random word in each new histogram to create a new list of words.
+    
+#     word_list : list
+#     amount : integer"""
 
-    return sentence
+#     sentence = []
+#     starting_histogram = Dictogram(word_list)
+#     next_word = starting_histogram.sample()
+#     sentence.append(next_word)
+#     for index in range((amount) - 1):
+#         chain = next_chain(word_list, next_word)
+#         if len(chain) > 0:
+#             next_word = chain.sample()
+#             sentence.append(next_word)
 
-def make_sentence(word_list):
-    """Takes each word in the list and joins them together into a single sentence + period at the end.
-
-    word_list : list"""
-
-    word_list[0] = word_list[0].capitalize()
-    final_sentence = ' '.join(word_list) + '.'
-    return final_sentence
+#     return sentence
 
 if __name__ == '__main__':
     word_list = ['one', 'fish', 'two', 'fish', 'red', 'fish', 'blue', 'fish', 'cat']
-    print(make_sentence(traverse(word_list, 15)))
+    markov = Markov(word_list, 20)
+    print(markov.main())
--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from pymongo import MongoClient
 # from Code.sample import sample_weight
 # from Code.tokenize import read_file
 from Code.dictogram import Dictogram, read_file
-from Code.markov_chain import make_sentence, traverse
+from Code.markov_chain import Markov
 import os
 
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Tweet-Generator')
@@ -17,10 +17,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """returns user to the homepage"""
-    word_doc = 'Code/txtdocs/sherlock.txt'
-    words = read_file(word_doc)
-    histogram = Dictogram(words)
-    sentence = histogram.get_sentence(15)
+    word_doc = 'Code/txtdocs/poe.txt'
+    # words = read_file(word_doc)
+    markov = Markov(read_file(word_doc), 20)
+    sentence = markov.main()
+    # histogram = Dictogram(words)
+    # sentence = histogram.get_sentence(15)
     return render_template("home.html", tweet=sentence)
 
 if __name__ == "__main__":
